e107c39ba98b599731db5c449733b26b18ecab58
LUIZ FERNANDO BILL DOS SANTOS <luizbillsantos@live.com>
2021-07-13 13:55:00 -0300

    Create UWP Icon and Splash

--- AppQuantidade/AppQuantidade.Android/SplashActivity.cs
+++ AppQuantidade/AppQuantidade.Android/SplashActivity.cs
@@ -12,7 +12,7 @@ using System.Text;
 
 namespace AppQuantidade.Droid
 {
-    [Activity(Label = "App Quantidade", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class SplashActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
